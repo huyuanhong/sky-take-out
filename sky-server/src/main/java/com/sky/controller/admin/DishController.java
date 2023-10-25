80baea30719482eaf539947953b556368a31b229
huyuanhong <huyuanhong2000@163.com>
2023-10-25 22:43:03 +0800

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,6 +11,7 @@ import com.sky.vo.DishVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
@@ -42,6 +43,32 @@ public class DishController {
         log.info("新增菜品：{}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
 
+        return Result.success();
+    }
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("菜品分页查询")
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
+        log.info("菜单分页查询：{}",dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 菜品批量删除
+     * @param ids
+     * @return
+     */
+    @DeleteMapping()
+    @ApiOperation("菜品批量删除")
+    public Result delete(@RequestParam List<Long> ids){
+        log.info("菜品批量删除：{}",ids);
+        dishService.deleteBatch(ids);
         return Result.success();
     }
 }
--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -10,5 +11,15 @@ import java.util.List;
  */
 @Mapper
 public interface DishFlavorMapper {
+    /**
+     * 批量插入口味数据
+     * @param flavors
+     */
     void insertBatch(List<DishFlavor> flavors);
+    @Delete("delete from sky_take_out.dish_flavor where dish_id = #{dishId}")
+    /**
+     * 批量删除口味数据
+     * @param flavors
+     */
+    void deleteByDishId(Long dishId);
 }
